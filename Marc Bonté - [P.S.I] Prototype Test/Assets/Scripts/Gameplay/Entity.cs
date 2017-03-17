fdc834a2d17af272d052a8f6f1dca3c53cb6567d
Marc <m.bonte@rubika-edu.com>
2017-03-17 12:20:03 +0100

    First commit
    - Spaceship controls
    - Spaceship's position constrained to camera

---
+++ Marc Bonté - [P.S.I] Prototype Test/Assets/Scripts/Gameplay/Entity.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Entity : MonoBehaviour
+{
+    private const float BORDER_MARGIN = 0.5f;
+    protected Rigidbody m_EntityRigidbody;
+
+    protected void Awake()
+    {
+        InitialiseRigidbody();
+    }
+
+    private void InitialiseRigidbody()
+    {
+        m_EntityRigidbody = GetComponent<Rigidbody>();
+        m_EntityRigidbody.useGravity = false;
+    }
+
+    protected virtual void Update()
+    {
+        ConstrainPositionToCamera();
+    }
+
+    private void ConstrainPositionToCamera()
+    {
+        // Horizontal constrain
+        if (transform.position.x < -Camera.main.orthographicSize)
+        {
+            transform.position = new Vector3(Camera.main.orthographicSize - BORDER_MARGIN, 0f, transform.position.z);
+        }
+        else if (transform.position.x > Camera.main.orthographicSize)
+        {
+            transform.position = new Vector3(-Camera.main.orthographicSize + BORDER_MARGIN, 0f, transform.position.z);
+        }
+
+        // Vertical constrain
+        if (transform.position.z < -Camera.main.orthographicSize)
+        {
+            transform.position = new Vector3(transform.position.x, 0f, Camera.main.orthographicSize - BORDER_MARGIN);
+        }
+        else if (transform.position.z > Camera.main.orthographicSize)
+        {
+            transform.position = new Vector3(transform.position.x, 0f, -Camera.main.orthographicSize + BORDER_MARGIN);
+        }
+    }
+}
---
+++ Marc Bonté - [P.S.I] Prototype Test/Assets/Scripts/Gameplay/Spaceship.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class SpaceshipControls
+{
+    public string rotation = "Horizontal";
+    public string moveForward = "MoveForward";
+    public string shoot = "Shoot";
+}
+
+[System.Serializable]
+public class SpaceshipParameters
+{
+    public float accelerationAmount = 1f;
+    public float maxVelocityMagnitude = 1f;
+    public float rotationSpeed = 1f;
+}
+
+public class Spaceship : Entity
+{
+    #region Variables
+    [SerializeField]
+    private SpaceshipControls m_Controls = new SpaceshipControls();
+    [SerializeField]
+    private SpaceshipParameters m_Parameters = new SpaceshipParameters();
+
+    private Vector3 m_Acceleration;
+    #endregion Variables
+
+    protected override void Update()
+    {
+        base.Update();
+
+        // Inputs
+        RotateSpaceship();
+        MoveSpaceship();
+
+        ApplyForces();
+    }
+
+    private void RotateSpaceship()
+    {
+        float rotationDirection = Input.GetAxisRaw(m_Controls.rotation);
+
+        if (rotationDirection != 0)
+        {
+            Quaternion targetRotation = Quaternion.Euler(transform.eulerAngles + (rotationDirection * (transform.up * m_Parameters.rotationSpeed)));
+            Quaternion newRotation = Quaternion.Slerp(transform.rotation, targetRotation, m_Parameters.rotationSpeed * Time.deltaTime);
+
+            m_EntityRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    private void MoveSpaceship()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SetAcceleration(transform.InverseTransformDirection(transform.forward));
+        }
+    }
+
+    private void SetAcceleration(Vector3 force)
+    {
+        m_Acceleration += force * m_Parameters.accelerationAmount;
+    }
+
+    private void ApplyForces()
+    {
+        m_EntityRigidbody.AddForce(transform.TransformDirection(m_Acceleration));
+        m_EntityRigidbody.velocity = Vector3.ClampMagnitude(m_EntityRigidbody.velocity, m_Parameters.maxVelocityMagnitude);
+
+        m_Acceleration = Vector3.zero;
+    }
+}
